84de55f6201167bf2f48240a5691b2531ca289f3
Michel Jansen <mjansen@whero.xyz>
2023-11-05 15:31:30 +0000

    Add Docker Compose deployment files

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
 cd /code/webapp
-# Apply database migrations
-echo "Apply database migrations"
-pipenv run python manage.py makemigrations --settings=vokou.settings.development
-pipenv run python manage.py migrate --settings=vokou.settings.development
-
 
 # Start server
 echo "Starting server"
-pipenv run python manage.py runserver 0.0.0.0:8000 --settings=vokou.settings.development 
+pipenv run python manage.py runserver 0.0.0.0:8000 --settings=$VOKO_ENV
